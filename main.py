062921f1337a61d29ed7c0e7f4ccfa41c093f9be
vjgarciag96 <vjgarciag96@gmail.com>
2018-02-16 17:12:16 +0100

    Implemented code to deploy at Heroku

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from telegram.ext import CommandHandler, Updater
 from commands import start
+import os
 
-TOKEN = 'your_bot_token'
+TOKEN = os.environ.get('TOKEN')
+PORT = int(os.environ.get('PORT', '8443'))
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
 if __name__ == 'main':
     updater = Updater(token=TOKEN)
@@ -10,4 +13,8 @@ if __name__ == 'main':
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.set_webhook(WEBHOOK_URL + TOKEN)
+    updater.idle()
